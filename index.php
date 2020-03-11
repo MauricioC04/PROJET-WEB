ebbb9d5ff49e1d3a6b12c941422e0562eb863798
Mauricio <mauricio.costa-cabral@cpnv.ch>
2020-03-11 13:45:15 +0100

    Création Page d'accueil et gabarit

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Mauricio COSTA CABRAL
+ * Date: 06.03.2020
+ * Version: 0.1
+ */
+
+session_start();
+require 'controler/controler.php';
+
+if(isset($_GET['action'])){
+    $action = $_GET['action'];
+    switch($action){
+        case'home':
+            home();
+            break;
+        default:
+            home();
+            break;
+    }
+}
+else{
+    home();
+}
